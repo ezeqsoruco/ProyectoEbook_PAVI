223a56d8250535142dc0c780117d29d9f6d384d6
ezeqsoruco <38668672+ezeqsoruco@users.noreply.github.com>
2020-04-20 21:26:13 -0300

    Login
    Se diseñó el login

--- Ebook/LoginEbook.Designer.cs
+++ Ebook/LoginEbook.Designer.cs
@@ -28,55 +28,192 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginEbook));
             this.txtUsuarioLogin = new System.Windows.Forms.TextBox();
             this.txtContraseniaLogin = new System.Windows.Forms.TextBox();
-            this.lblNombreUsuario = new System.Windows.Forms.Label();
-            this.lblContraseniaUsuario = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.shapeContainer1 = new Microsoft.VisualBasic.PowerPacks.ShapeContainer();
+            this.lineShape1 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.lineShape2 = new Microsoft.VisualBasic.PowerPacks.LineShape();
+            this.label1 = new System.Windows.Forms.Label();
+            this.linkPass = new System.Windows.Forms.LinkLabel();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnMinimizar = new System.Windows.Forms.PictureBox();
+            this.btnCerrar = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // txtUsuarioLogin
             // 
-            this.txtUsuarioLogin.Location = new System.Drawing.Point(173, 67);
+            this.txtUsuarioLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.txtUsuarioLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtUsuarioLogin.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuarioLogin.ForeColor = System.Drawing.Color.DimGray;
+            this.txtUsuarioLogin.Location = new System.Drawing.Point(352, 93);
             this.txtUsuarioLogin.Name = "txtUsuarioLogin";
-            this.txtUsuarioLogin.Size = new System.Drawing.Size(157, 20);
-            this.txtUsuarioLogin.TabIndex = 0;
+            this.txtUsuarioLogin.Size = new System.Drawing.Size(356, 20);
+            this.txtUsuarioLogin.TabIndex = 1;
+            this.txtUsuarioLogin.Text = "USUARIO";
+            this.txtUsuarioLogin.Enter += new System.EventHandler(this.txtUsuarioLogin_Enter);
+            this.txtUsuarioLogin.Leave += new System.EventHandler(this.txtUsuarioLogin_Leave);
             // 
             // txtContraseniaLogin
             // 
-            this.txtContraseniaLogin.Location = new System.Drawing.Point(173, 109);
+            this.txtContraseniaLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.txtContraseniaLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtContraseniaLogin.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtContraseniaLogin.ForeColor = System.Drawing.Color.DimGray;
+            this.txtContraseniaLogin.Location = new System.Drawing.Point(350, 148);
             this.txtContraseniaLogin.Name = "txtContraseniaLogin";
-            this.txtContraseniaLogin.Size = new System.Drawing.Size(157, 20);
-            this.txtContraseniaLogin.TabIndex = 1;
+            this.txtContraseniaLogin.Size = new System.Drawing.Size(356, 20);
+            this.txtContraseniaLogin.TabIndex = 2;
+            this.txtContraseniaLogin.Text = "CONTRASEÑA";
+            this.txtContraseniaLogin.Enter += new System.EventHandler(this.txtContraseniaLogin_Enter);
+            this.txtContraseniaLogin.Leave += new System.EventHandler(this.txtContraseniaLogin_Leave);
             // 
-            // lblNombreUsuario
+            // panel1
             // 
-            this.lblNombreUsuario.AutoSize = true;
-            this.lblNombreUsuario.Location = new System.Drawing.Point(85, 70);
-            this.lblNombreUsuario.Name = "lblNombreUsuario";
-            this.lblNombreUsuario.Size = new System.Drawing.Size(82, 13);
-            this.lblNombreUsuario.TabIndex = 2;
-            this.lblNombreUsuario.Text = "Ingrese usuario:";
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(250, 330);
+            this.panel1.TabIndex = 2;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
             // 
-            // lblContraseniaUsuario
+            // shapeContainer1
             // 
-            this.lblContraseniaUsuario.AutoSize = true;
-            this.lblContraseniaUsuario.Location = new System.Drawing.Point(69, 112);
-            this.lblContraseniaUsuario.Name = "lblContraseniaUsuario";
-            this.lblContraseniaUsuario.Size = new System.Drawing.Size(101, 13);
-            this.lblContraseniaUsuario.TabIndex = 3;
-            this.lblContraseniaUsuario.Text = "Ingrese contraseña:";
+            this.shapeContainer1.Location = new System.Drawing.Point(0, 0);
+            this.shapeContainer1.Margin = new System.Windows.Forms.Padding(0);
+            this.shapeContainer1.Name = "shapeContainer1";
+            this.shapeContainer1.Shapes.AddRange(new Microsoft.VisualBasic.PowerPacks.Shape[] {
+            this.lineShape2,
+            this.lineShape1});
+            this.shapeContainer1.Size = new System.Drawing.Size(780, 330);
+            this.shapeContainer1.TabIndex = 3;
+            this.shapeContainer1.TabStop = false;
             // 
-            // Form1
+            // lineShape1
+            // 
+            this.lineShape1.BorderColor = System.Drawing.Color.Gray;
+            this.lineShape1.Enabled = false;
+            this.lineShape1.Name = "lineShape1";
+            this.lineShape1.X1 = 352;
+            this.lineShape1.X2 = 707;
+            this.lineShape1.Y1 = 118;
+            this.lineShape1.Y2 = 118;
+            // 
+            // lineShape2
+            // 
+            this.lineShape2.BorderColor = System.Drawing.Color.Gray;
+            this.lineShape2.Enabled = false;
+            this.lineShape2.Name = "lineShape2";
+            this.lineShape2.X1 = 350;
+            this.lineShape2.X2 = 705;
+            this.lineShape2.Y1 = 172;
+            this.lineShape2.Y2 = 172;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ScrollBar;
+            this.label1.Location = new System.Drawing.Point(488, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(77, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "LOGIN";
+            // 
+            // linkPass
+            // 
+            this.linkPass.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(122)))), ((int)(((byte)(204)))));
+            this.linkPass.AutoSize = true;
+            this.linkPass.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkPass.LinkColor = System.Drawing.SystemColors.ScrollBar;
+            this.linkPass.Location = new System.Drawing.Point(422, 252);
+            this.linkPass.Name = "linkPass";
+            this.linkPass.Size = new System.Drawing.Size(203, 17);
+            this.linkPass.TabIndex = 0;
+            this.linkPass.TabStop = true;
+            this.linkPass.Text = "¿Has olvidado la contraseña?";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnLogin.FlatAppearance.BorderSize = 0;
+            this.btnLogin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogin.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.ForeColor = System.Drawing.Color.LightGray;
+            this.btnLogin.Location = new System.Drawing.Point(352, 197);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(356, 40);
+            this.btnLogin.TabIndex = 3;
+            this.btnLogin.Text = "INGRESAR";
+            this.btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // btnMinimizar
+            // 
+            this.btnMinimizar.Image = global::Ebook.Properties.Resources.icons8_minimizar_la_ventana_48;
+            this.btnMinimizar.Location = new System.Drawing.Point(702, 3);
+            this.btnMinimizar.Name = "btnMinimizar";
+            this.btnMinimizar.Size = new System.Drawing.Size(30, 30);
+            this.btnMinimizar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnMinimizar.TabIndex = 6;
+            this.btnMinimizar.TabStop = false;
+            this.btnMinimizar.Click += new System.EventHandler(this.btnMinimizar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Image = global::Ebook.Properties.Resources.icons8_cerrar_ventana_48;
+            this.btnCerrar.Location = new System.Drawing.Point(738, 3);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(30, 30);
+            this.btnCerrar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnCerrar.TabIndex = 5;
+            this.btnCerrar.TabStop = false;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Ebook.Properties.Resources.libro;
+            this.pictureBox3.Location = new System.Drawing.Point(37, 93);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(167, 135);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 0;
+            this.pictureBox3.TabStop = false;
+            // 
+            // LoginEbook
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(488, 261);
-            this.Controls.Add(this.lblContraseniaUsuario);
-            this.Controls.Add(this.lblNombreUsuario);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(15)))), ((int)(((byte)(15)))));
+            this.ClientSize = new System.Drawing.Size(780, 330);
+            this.Controls.Add(this.btnMinimizar);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.linkPass);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.txtContraseniaLogin);
             this.Controls.Add(this.txtUsuarioLogin);
-            this.Name = "Form1";
+            this.Controls.Add(this.shapeContainer1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LoginEbook";
+            this.Opacity = 0.9D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Login";
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.LoginEbook_MouseDown);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.btnMinimizar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnCerrar)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -86,8 +223,16 @@
 
         private System.Windows.Forms.TextBox txtUsuarioLogin;
         private System.Windows.Forms.TextBox txtContraseniaLogin;
-        private System.Windows.Forms.Label lblNombreUsuario;
-        private System.Windows.Forms.Label lblContraseniaUsuario;
+        private System.Windows.Forms.Panel panel1;
+        private Microsoft.VisualBasic.PowerPacks.ShapeContainer shapeContainer1;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape2;
+        private Microsoft.VisualBasic.PowerPacks.LineShape lineShape1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkPass;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.PictureBox btnCerrar;
+        private System.Windows.Forms.PictureBox btnMinimizar;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
--- Ebook/LoginEbook.cs
+++ Ebook/LoginEbook.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace Ebook
 {
@@ -16,5 +17,78 @@ namespace Ebook
         {
             InitializeComponent();
         }
+
+        //Código necesario para que la venta pueda moverse
+        [DllImport("user32.DLL", EntryPoint = "ReleaseCapture")]
+        private extern static void ReleaseCapture();
+        [DllImport("user32.DLL", EntryPoint = "SendMessage")]
+        private extern static void SendMessage(System.IntPtr hwnd, int wmsg, int wparam, int lparam);
+        //
+
+        //Evento cuando el cursor está dentro del textbox
+        private void txtUsuarioLogin_Enter(object sender, EventArgs e)
+        {
+            if (txtUsuarioLogin.Text == "USUARIO")
+            {
+                txtUsuarioLogin.Text = "";
+                txtUsuarioLogin.ForeColor = Color.LightGray;
+            }
+        }
+
+        //Evento cuando el cursor está fuera del textbox
+        private void txtUsuarioLogin_Leave(object sender, EventArgs e)
+        {
+            if (txtUsuarioLogin.Text == "")
+            {
+                txtUsuarioLogin.Text = "USUARIO";
+                txtUsuarioLogin.ForeColor = Color.DimGray;
+            }
+        }
+
+        //Evento cuando el cursor está dentro del textbox
+        private void txtContraseniaLogin_Enter(object sender, EventArgs e)
+        {
+            if (txtContraseniaLogin.Text == "CONTRASEÑA")
+            {
+                txtContraseniaLogin.Text = "";
+                txtContraseniaLogin.ForeColor = Color.LightGray;
+                txtContraseniaLogin.UseSystemPasswordChar = true;
+            }
+        }
+
+        //Evento cuando el cursor está fuera del textbox
+        private void txtContraseniaLogin_Leave(object sender, EventArgs e)
+        {
+            if(txtContraseniaLogin.Text == "")
+            {
+                txtContraseniaLogin.Text = "CONTRASEÑA";
+                txtContraseniaLogin.ForeColor = Color.DimGray;
+                txtContraseniaLogin.UseSystemPasswordChar = false;
+            }
+        }
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnMinimizar_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        //Evento creado necesario para que la vena pueda moverse
+        private void LoginEbook_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            ReleaseCapture();
+            SendMessage(this.Handle, 0x112, 0xf012, 0);
+        }
+        //
     }
 }
